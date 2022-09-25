c547b411e02cd4e04b859703eb82161b2133eaf3
ahmed2100 <ahmed2100123@gmail.com>
2022-09-25 09:42:34 +0200

    EUSART module added (Ahmed Odb)

--- application.c
+++ application.c
@@ -1,6 +1,6 @@
 /*
  * File:   application.c
- * Author: ahmed radwan
+ * Author: ahmed Odb
  *
  * Created on September 16, 2022, 7:48 PM
  */
@@ -8,39 +8,29 @@
 #include"application.h"
 
 #include "application.h"
-#include "MCAL/I2C/mcal_i2c.h"
-#include"ECU/SERVO_MOTOR_CONTROL_BY_I2C/ecu_servo_motor_i2c.h"
+#include "MCAL/EUSART/mcal_EUSART.h"
 
-mssp_i2c_st i2c_obj={
-  .i2c_cfg.i2c_mode=  I2C_MSSP_MASTER_MODE,
-  .i2c_cfg.i2c_mode_cfg=I2C_MASTER_MODE_DEFINED_CLOCK,
-  .i2c_clock=100000,
-  .i2c_cfg.i2c_SMBus_control=I2C_SMBus_DISABLE,
-  .i2c_cfg.i2c_slew_rate=I2C_SLEW_RATE_DISABLE,
-  .I2C_DefaultInterruptHandler=NULL,
-  .I2C_Report_Receive_Overflow=NULL,
-  .I2C_Report_Write_Collision=NULL
+uart_config_t uart = {
+  .baud_rate_config = BAUDRATE_ASYNC_8BIT_LOW_SPEED ,
+  .uart_baud_rate_reg_value = 223 ,
+  .tx_config.tx_9th_bit_en = EUSART_ASYNCH_Tx_BIT_9_DISABLED ,
+  .tx_config.tx_enable = EUSART_ASYNCH_TX_EN ,
+  .rx_config.rx_9th_bit_en = EUSART_ASYNCH_Tx_BIT_9_DISABLED ,
+  .rx_config.rx_enable = EUSART_ASYNCH_RX_EN 
 };
-servo_driver_st s1_obj={
-   .slave_address=SERVO_DRIVER_SLAVE_ADDRESS,
-   .frequancy=ECU_SM_PRE_SCALE_REG_VAL,
-   .mode_1_cfg=0x21,
-   .mode_2_cfg=0x04   
-};
-#define delay 100
-uint8 angle=0;
+
+
+uint16_t data = 0 ;
+
 int main()
 {
     application_intialize();
 
-    for(int i=0;i<16;i++)
-    {
-        Servo_SetAngle(&i2c_obj,&s1_obj,i,angle);
-        //__delay_ms(delay);
-        angle+=15;
-    }
      while(1)
      {
+         
+         EUSART_Async_Transmit_Data(&uart , 'A');         
+         EUSART_Async_Read_Data(&uart , &data );
          
      }    
     return 0;
@@ -48,6 +38,6 @@ int main()
 }
 void application_intialize(void)
 {
-    MSSP_I2C_Init(&i2c_obj);
-    ServoDriver_Init(&i2c_obj,&s1_obj);
+    EUSART_Async_Init(&uart);
+    
 }
