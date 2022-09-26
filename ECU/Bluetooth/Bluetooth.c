0944877606923fbdad93106e560e898cd33d023a
ahmed2100 <38797485+ahmed2100@users.noreply.github.com>
2022-09-26 07:38:58 +0200

    Bluetooth Module
    only init() needed

---
+++ ECU/Bluetooth/Bluetooth.c
@@ -0,0 +1,83 @@
+/* 
+ * File:   Bluetooth.h
+ * Author: Ahmed
+ *
+ * Created on September 26, 2022, 7:22 AM
+ */
+
+#include "Bluetooth.h"
+
+
+
+
+Std_ReturnType Bluetooth_Init(const uart_config_st *_uart_obj)
+{
+    
+}
+
+Std_ReturnType Bluetooth_Send_Data_Blocking(const uart_config_st *_uart_obj , uint8_t data)
+{
+    Std_ReturnType ret_val = E_OK ;
+    
+    if((NULL ==_uart_obj ))
+    {
+        ret_val = E_NOT_OK ;
+    }
+    else 
+    {
+        EUSART_Async_Transmit_Data_Blocking(_uart_obj , data);
+    }
+    
+    return ret_val ;
+}
+Std_ReturnType Bluetooth_Recieve_Data_Blocking(const uart_config_st *_uart_obj , uint8_t *data )
+{
+    
+    Std_ReturnType ret_val = E_OK ;
+    
+    if((NULL ==_uart_obj )|| (NULL == data) )
+    {
+        ret_val = E_NOT_OK ;
+    }
+    else 
+    {
+        EUSART_Async_Read_Data_Blocking(_uart_obj , data);
+    }
+    
+    return ret_val ;  
+}
+
+Std_ReturnType Bluetooth_Send_Data_Non_Blocking(const uart_config_st *_uart_obj , uint8_t data )
+{
+    
+    Std_ReturnType ret_val = E_OK ;
+    
+    if((NULL ==_uart_obj ) )
+    {
+        ret_val = E_NOT_OK ;
+    }
+    else 
+    {
+        EUSART_Async_Transmit_Data(_uart_obj , data);
+    }
+    
+    return ret_val ;
+    
+}
+Std_ReturnType Bluetooth_Recieve_Data_Non_Blocking(const uart_config_st *_uart_obj , uint8_t *data )
+{
+    
+    Std_ReturnType ret_val = E_OK ;
+    
+    if((NULL ==_uart_obj ) || (NULL == data))
+    {
+        ret_val = E_NOT_OK ;
+    }
+    else 
+    {
+        EUSART_Async_Read_Data(_uart_obj , data);
+    }
+    
+    return ret_val ;
+    
+}
